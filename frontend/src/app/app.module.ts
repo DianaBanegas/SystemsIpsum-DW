b311528921c5b006f2ec3c2828397b80db422e7a
DianaBanegas <42158845+DianaBanegas@users.noreply.github.com>
2021-04-12 17:48:51 -0600

    .

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { PlanindexComponent } from './components/planindex/planindex.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { TopBarProjectComponent } from './components/top-bar-project/top-bar-project.component';
 import { ProjectComponent } from './components/project/project.component';
+import { UserComponent } from './user/user.component';
 
 
 @NgModule({
@@ -47,7 +48,8 @@ import { ProjectComponent } from './components/project/project.component';
     PlanindexComponent,
     SidebarComponent,
     TopBarProjectComponent,
-    ProjectComponent
+    ProjectComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
